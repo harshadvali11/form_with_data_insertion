c31c64563c55b00fa169bf97950946ec653cb0d8
harshadvali11 <harshadvali1431@gmail.com>
2023-01-28 15:29:59 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from app.models import *
+from django.http import HttpResponse
+# Create your views here.
+
+def insert_topic(request):
+    if request.method=="POST":
+        tn=request.POST['topic']
+        T=Topic.objects.get_or_create(topic_name=tn)[0]
+        T.save()
+        return HttpResponse('insert_topic is done')
+    return render(request,'insert_topic.html')
